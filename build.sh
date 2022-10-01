afbe80af702042b5ea0786a8a24c4646b948e2d2
MATSUU Takuto <matsuu@gmail.com>
2022-10-01 20:02:37 +0900

    Fix docker tag

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ BRANCH="master"
 if [ $# -ne 0 ]; then
   BRANCH="$1"
 fi
-TAG="neovim-${BRANCH}"
+TAG="build-neovim:${BRANCH}"
 BINARY="nvim-${BRANCH}.appimage"
 
 docker image build --build-arg BRANCH="${BRANCH}" -t "${TAG}" .
-docker run --rm neovim cat /nvim.appimage > "${BINARY}"
+docker run --rm "${TAG}" cat /nvim.appimage > "${BINARY}"
 chmod +x "${BINARY}"
 docker image rm "${TAG}"
